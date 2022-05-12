530bb531f1d44e57f9ade16b6db34042ea64b1b9
Asko Nõmm <ano@ano.ee>
2022-05-12 21:45:16 +0300

    Add java.util.Date to SCI

--- src/bruno/core.clj
+++ src/bruno/core.clj
@@ -236,7 +236,8 @@
               'format-date format-date}
    :namespaces {'clojure.core {'prn prn
                                'println println}
-                'clojure.string {'split string/split}}})
+                'clojure.string {'split string/split}}}
+  :classes {'java.util.Date java.util.Date})
 
 
 (defn build-content-items!
